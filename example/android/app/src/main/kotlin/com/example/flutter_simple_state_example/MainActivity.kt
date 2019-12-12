772ecc4e5a739add95a79da79725a6719006a03e
Waldemar Kornewald <wkornewald@gmail.com>
2019-12-12 10:06:01 +0100

    Upgrade to Flutter 1.12 / Dart 2.7

---
+++ example/android/app/src/main/kotlin/com/example/flutter_simple_state_example/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.flutter_simple_state_example
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
